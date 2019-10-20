abb1e543d88fb6f6421eaa799a8b8da3b0215533
ReginaldJbeili <53199009+ReginaldJbeili@users.noreply.github.com>
2019-10-20 16:45:58 +0100

    placed functions to make calculator working

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-Javascript
+function insert(num){
+document.form.textview.value+=num;
+}
+
+function solve() {
+document.form.textview.value = eval(document.form.textview.value);
+}
+
+function clean(){
+document.form.textview.value = "";
+}
+
+function back() {
+var x = document.form.textview.value
+document.form.textview.value = x.substring(0,x.length-1);
+}
